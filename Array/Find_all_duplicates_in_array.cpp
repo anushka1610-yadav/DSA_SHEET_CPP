21f736036aeebe90a3efdb3cd7e1056642cc181c
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-05-24 15:22:07 +0530

    added

--- Array/Find_all_duplicates_in_array.cpp
+++ Array/Find_all_duplicates_in_array.cpp
@@ -1,17 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         unordered_map<int,int>mp;
+//         vector<int>ans;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+//         for(auto i=mp.begin();i!=mp.end();i++)
+//         {
+//             if(i->second > 1)
+//             {
+//                 ans.push_back(i->first);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int>mp;
         vector<int>ans;
         for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-        }
-        for(auto i=mp.begin();i!=mp.end();i++)
-        {
-            if(i->second > 1)
+            int ind = abs(nums[i])-1;
+            int val = nums[ind];
+            
+            if(val<0)
             {
-                ans.push_back(i->first);
+                ans.push_back(ind+1);
+            }
+            else {
+                nums[ind] *= -1;
             }
         }
         return ans;
